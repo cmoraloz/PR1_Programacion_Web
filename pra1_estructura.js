e8ee7a5a885c1a2cba3b0ac46b7790b79818c8d9
cmoraloz <cmoraloz@uoc.edu>
2025-04-02 20:09:36 +0200

    Update pra1_estructura.js

--- pra1_estructura.js
+++ pra1_estructura.js
@@ -26,19 +26,23 @@ class Film {
 }
 
 //2. Creación de la clase FilmList.
+//Esta clase reprensenta una lista de películas y contiene varios métodos para gestionarla.
 
 class FilmList {
     constructor() {
-        
+        this.film = []; //Inicializamos la lista como un array vacío.
     }
 
-    addFilm (film){
-
-    };
+//2.1 Método para agregar una película a la lista.
     
+    addFilm (film){
+        this.film.push(film); //Añadimos la película al array.
+    }
+
+//2.2 Método para eliminar una película de la lista por su ID.
 
     removeFilm (filmId) {
-        
+        this.films = this.films.filtr(film => film.id !== filmId); //Filtramos las películas que no coincidan por su ID.
     }
 
     showList() {
